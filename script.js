0c13e7339bece1c585cd568f5c003fb24ebda7ce
Ryan Kelly <rynklly@gmail.com>
2021-10-13 09:52:18 -0700

    polishing

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const gridBox = document.querySelector('.gridBox');
-const screenSize = 980;
+const screenSize = 500;
 let gridCount = 50;
-let gridSize = 980/gridCount;
+let gridSize = 500/gridCount;
 
 function drawGrid () {
     for (i=1 ; i <= gridCount**2 ; i++) {
@@ -23,3 +23,31 @@ for (i=0 ; i <= gridSquare.length - 1 ; i++) {
 for (i=0 ; i <= gridSquare.length - 1 ; i++) {
     gridSquare[i].addEventListener('mouseover', function () {this.classList.add('hovered')});
 };
+
+let reset = document.querySelector('#reset');
+
+reset.addEventListener('click', () => {
+    gridCount = Math.round(Number(prompt("Choose your resolution (from 9-64)")));
+    gridBox.innerHTML = " ";
+    if (gridCount <=64 && gridCount >= 9) {
+        drawGrid();
+
+        gridSize = 500/gridCount;
+        let gridSquare = Array.from(document.getElementsByClassName('gridSquare'));
+        for (i=0 ; i <= gridSquare.length - 1 ; i++) {
+            gridSquare[i].style.width = gridSize + 'px';
+            gridSquare[i].style.height = gridSize + 'px';
+    
+        }
+
+
+        for (i=0 ; i <= gridSquare.length - 1 ; i++) {
+            gridSquare[i].addEventListener('mouseover', function () {this.classList.add('hovered')});
+        };
+       
+
+    } else {
+         alert("Enter a valid number!");
+        return;
+    }
+})
